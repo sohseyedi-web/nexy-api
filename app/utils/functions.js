2952832db6b04c3a97155d007a668667efd54207
sohseyedi <sohseyedi@gmail.com>
2025-05-12 15:44:18 +0330

    feat: implement JWT token generation and verification functions

---
+++ app/utils/functions.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const generateToken = (payload) => {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: "7d" });
+};
+
+const verifyToken = (token) => {
+  return jwt.verify(token, JWT_SECRET);
+};
+
+module.exports = {
+  verifyToken,
+  generateToken,
+};
