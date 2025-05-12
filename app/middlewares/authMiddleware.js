da376cd4da24af7a0382099b959dc87884e73056
sohseyedi <sohseyedi@gmail.com>
2025-05-12 15:45:28 +0330

    feat: add authentication middleware to verify user tokens

---
+++ app/middlewares/authMiddleware.js
@@ -0,0 +1,16 @@
+const { verifyToken } = require("../utils/functions");
+
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  try {
+    const decoded = verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = authMiddleware;
