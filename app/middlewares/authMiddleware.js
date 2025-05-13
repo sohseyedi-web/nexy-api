05bcd6e62106aae5c51b768de452a19d1a61ea5a
sohseyedi <sohseyedi@gmail.com>
2025-05-13 11:59:36 +0330

    chore: enhance authMiddleware for improved token handling and error messages

--- app/middlewares/authMiddleware.js
+++ app/middlewares/authMiddleware.js
@@ -1,15 +1,21 @@
 const { verifyToken } = require("../utils/functions");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
+  const token = req.cookies?.token;
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "دسترسی غیرمجاز. توکن موجود نیست." });
+  }
 
   try {
     const decoded = verifyToken(token);
     req.user = decoded;
     next();
-  } catch {
-    return res.status(401).json({ message: "Invalid token" });
+  } catch (error) {
+    console.error("توکن نامعتبر:", error.message);
+    return res.status(401).json({ message: "توکن نامعتبر است." });
   }
 };
 
