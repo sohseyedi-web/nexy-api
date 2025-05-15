7ac70b9ee0d18434b4ffadd3199471da735f29b3
sohseyedi <sohseyedi@gmail.com>
2025-05-15 20:31:58 +0330

    chore: update SameSite attribute for cookies from 'strict' to 'lax'

--- app/controller/userController.js
+++ app/controller/userController.js
@@ -25,9 +25,10 @@ const login = async (req, res, next) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+
     res.status(200).json(user);
   } catch (err) {
     res.status(500).json({ message: "Login failed", error: err.message });
